c3f614e4675d4808cf8bbe3694154afa6adf6fc8
Pavel Boyko <boykopb@gmail.com>
2020-08-20 15:03:50 +0300

    20.08.2020 15.03

--- 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -2,31 +2,31 @@ package com.javarush.task.task20.task2022;
 
 
 import java.io.*;
-
-/*
+/* 
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    private transient FileOutputStream stream;
+    private String fileName;
 
     public Solution(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
         this.stream = new FileOutputStream(fileName);
     }
 
     public void writeObject(String string) throws IOException {
         stream.write(string.getBytes());
-        stream.write("\n".getBytes());
+        stream.write("\r\n".getBytes());
         stream.flush();
     }
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        in.close();
+        stream = new FileOutputStream(fileName, true);
     }
 
     @Override
@@ -35,12 +35,34 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Solution sol = new Solution("C:/1.txt");
+        sol.writeObject("Wright1");
+        sol.writeObject("Wright12");
+        sol.writeObject("Wright13");
 
 
+        String filePath = "C:/2.txt";
+        ObjectOutputStream streamOut = new ObjectOutputStream(new FileOutputStream(filePath));
+        //если попытаться тут у объекта запустить метод writeObject(ObjectOutputStream out) и
+        //передать туда наш стрим - получим ошибку NotActiveException - это не правильно.
+        streamOut.writeObject(sol);
+        //закрываем поток вывода в файл
+        sol.close();
+        //закрываем поток сериализации
+        streamOut.close();
 
+        ObjectInputStream streamIn = new ObjectInputStream(new FileInputStream(filePath));
+        Solution sol2 = (Solution) streamIn.readObject();
+        sol2.writeObject("newStream1");
+        sol2.writeObject("newStream2");
+        sol2.writeObject("newStream3");
 
-
+        //закрываем поток вывода в файл
+        sol2.close();
+        //закрываем поток десериализации
+        streamIn.close();
 
     }
 }
