aeee203fb2e4c738dddf6024cf3d618de5b4010e
Pavel Boyko <boykopb@gmail.com>
2020-08-29 10:50:36 +0300

    29.08.2020 10:50

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+
+    public void firstMethod ()  {
+        System.out.println("This is first method!");
+    }
+
+    public void secondMethod ()  {
+        System.out.println("This is second method!");
+    }
+
+}
