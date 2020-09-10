30caa4341e6e23b660eab19fea615dc522b8b4fb
Pavel Boyko <boyko@laspteh.ru>
2020-09-10 13:48:58 +0300

    10.09.2020 13.48

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,45 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
-    public static void main(String[] args) {
-        
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server started");
+
+            while (true) {
+                Socket newSocket = serverSocket.accept();
+                Handler handler = new Handler(newSocket);
+                handler.start();
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> map : connectionMap.entrySet()) {
+            try {
+                map.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("Can't send message to : " + map.getKey());
+            }
+        }
     }
 }
