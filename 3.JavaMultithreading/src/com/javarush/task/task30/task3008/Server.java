51ff28f83945231fb77ee7062172232db7ec5b56
Pavel Boyko <boykopb@gmail.com>
2020-09-11 10:20:39 +0300

    11.09.2020 10.20

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -24,31 +24,57 @@ public class Server {
                 connection.send(request);
                 answer = connection.receive();
                 userName = answer.getData();
-
-            } while (answer.getType() != MessageType.USER_NAME || userName.isEmpty() || connectionMap.containsKey(userName));
+            }
+            while (answer.getType() != MessageType.USER_NAME || userName.isEmpty() || connectionMap.containsKey(userName));
             connectionMap.put(userName, connection);
-            connection.send(new Message(MessageType.NAME_ACCEPTED, " Вы в чате!"));
+            connection.send(new Message(MessageType.NAME_ACCEPTED, " присоединился к чату!"));
             return userName;
         }
-
 
         private void notifyUsers(Connection connection, String userName) throws IOException {
             for (Map.Entry<String, Connection> map : connectionMap.entrySet()) {
                 if (!userName.equals(map.getKey())) {
                     connection.send(new Message(MessageType.USER_ADDED, map.getKey()));
                 }
-
             }
-
         }
 
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while(true) {
+                Message chatting = connection.receive();
+                if (chatting.getType() == MessageType.TEXT) {
+                    Server.sendBroadcastMessage(new Message(MessageType.TEXT, userName + ": " + chatting.getData()));
+                }
+                if (chatting.getType() != MessageType.TEXT) {
+                    ConsoleHelper.writeMessage("Тип сообщения не \"TEXT\". Отправка не возможна.");
+                }
+            }
+        }
 
+        @Override
+        public void run() {
+            ConsoleHelper.writeMessage("Установлено соединение с сервером: " + socket.getRemoteSocketAddress());
+            String userName;
+            try (Connection connection = new Connection(socket)) {
+                userName =serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, userName));
+                notifyUsers(connection, userName);
+                serverMainLoop(connection, userName);
+                connectionMap.remove(userName);
+                sendBroadcastMessage(new Message(MessageType.USER_REMOVED, userName));
+
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("Произошла ошибка при обмене данными с удаленным адресом.");
+            }
+
+
+        }
     }
 
     public static void main(String[] args) throws IOException {
         int port = ConsoleHelper.readInt();
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server started");
+            System.out.println("Сервер запущен");
 
             while (true) {
                 Socket newSocket = serverSocket.accept();
@@ -66,7 +92,7 @@ public class Server {
             try {
                 map.getValue().send(message);
             } catch (IOException e) {
-                System.out.println("Can't send message to : " + map.getKey());
+                System.out.println("Не могу отправить сообщение : " + map.getKey());
             }
         }
     }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+
+public class Client {
+    public class SocketThread extends Thread {
+
+    }
+
+
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    protected String getServerAddress() {
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        return ConsoleHelper.readString();
+    }
+
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) throws IOException {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка. Сообщение не отправлено.");
+            clientConnected = false;
+        }
+    }
+
+
+}
