de4646c5073823148f44b6325288a5a3ad16ced2
Pavel Boyko <boykopb@gmail.com>
2020-09-18 21:47:43 +0300

    18.09.2020 21.47

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
@@ -7,23 +7,22 @@ import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * Команда распаковки архива
- */
-
-public class ZipExtractCommand extends ZipCommand{
+public class ZipExtractCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
             ConsoleHelper.writeMessage("Распаковка архива.");
+
             ZipFileManager zipFileManager = getZipFileManager();
-            ConsoleHelper.writeMessage("Введите директорию для распаковки");
-            Path sourcePath = Paths.get(ConsoleHelper.readString());
-            zipFileManager.extractAll(sourcePath);
-            ConsoleHelper.writeMessage("Архив распакован.");
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
+
+            ConsoleHelper.writeMessage("Архив был распакован.");
 
         } catch (PathIsNotFoundException e) {
-            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
         }
     }
 }
