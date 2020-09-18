631e7fb5f16f886a74887b1747c3e30656c17699
Pavel Boyko <boykopb@gmail.com>
2020-09-18 09:16:19 +0300

    17.09.2020 9.00

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,59 +1,37 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
-        askOperation();
-      /*  BufferedReader keyboardreader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите полный путь к результату архивации");
-        String archivePath = keyboardreader.readLine();
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(archivePath));
-
-        System.out.println("Введите полный путь к файлу, который будем архивировать");
-        String filePath = keyboardreader.readLine();
-
-        keyboardreader.close();
-
-        zipFileManager.createZip(Paths.get(filePath));
-
-        ExitCommand exitCommand = new ExitCommand();
-
-        exitCommand.execute();*/
+        } while (operation != Operation.EXIT);
     }
 
-    public static Operation askOperation() throws IOException {
-        for(Operation operation : Operation.values())
-            ConsoleHelper.writeMessage(String.format("%s - %s", operation.ordinal(), operation));
 
-        Operation operationResult;
-        switch (ConsoleHelper.readInt()) {
-            case 0:
-                operationResult = Operation.CREATE;
-                break;
-            case 1:
-                operationResult = Operation.ADD;
-                break;
-            case 2:
-                operationResult = Operation.REMOVE;
-                break;
-            case 3:
-                operationResult = Operation.EXTRACT;
-                break;
-            case 4:
-                operationResult = Operation.CONTENT;
-                break;
-            default:
-                operationResult = Operation.EXIT;
-                break;
-        }
-        return operationResult;
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
