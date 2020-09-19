4f2526fb9abb2b77c1f15a57080ffc7eea3b2b5d
Pavel Boyko <boykopb@gmail.com>
2020-09-19 17:11:14 +0300

    19.09.2020 17.11

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 
 
+
 public class Archiver {
     public static void main(String[] args) throws IOException {
 
--- 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -2,8 +2,9 @@ package com.javarush.task.task31.task3105;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -11,7 +12,41 @@ import java.util.zip.ZipOutputStream;
 /* 
 Добавление файла в архив
 */
+//C:\qwert\Text.txt
+//C:\qwert\2.zip
+
 public class Solution {
+
     public static void main(String[] args) throws IOException {
+        String newFileName = Paths.get(args[0]).getFileName().toString();
+        String newPathInArchive = "new/" + newFileName;
+        ZipEntry entry;
+        Map<String, ByteArrayOutputStream> filesInArchive = new HashMap<>();
+
+        ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(args[1]));
+        while ((entry = zipInputStream.getNextEntry()) != null) {
+            if (entry.getName().endsWith(newFileName)) {
+                newPathInArchive = entry.getName();
+            } else {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                int length = 0;
+                byte[] buffer = new byte[1024];
+                while ((length = zipInputStream.read(buffer)) != -1) {
+                    byteArrayOutputStream.write(buffer, 0, length);
+                }
+                filesInArchive.put(entry.getName(), byteArrayOutputStream);
+            }
+        }
+        zipInputStream.close();
+
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(args[1]));
+        zipOutputStream.putNextEntry(new ZipEntry(newPathInArchive));
+        Files.copy(Paths.get(args[0]), zipOutputStream);
+        for (Map.Entry<String, ByteArrayOutputStream> pair : filesInArchive.entrySet()) {
+            zipOutputStream.putNextEntry(new ZipEntry(pair.getKey()));
+            pair.getValue().writeTo(zipOutputStream);
+        }
+        zipOutputStream.close();
     }
+
 }
