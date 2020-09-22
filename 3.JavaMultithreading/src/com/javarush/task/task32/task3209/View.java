e60f2a0fd1a1b240871f151e39863c26ef0c6dd4
Pavel Boyko <boykopb@gmail.com>
2020-09-22 14:37:33 +0300

    22.09.2020 14.40

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,11 +1,17 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
 
     public Controller getController() {
         return controller;
@@ -15,8 +21,42 @@ public class View extends JFrame implements ActionListener {
         this.controller = controller;
     }
 
+    /*
+    4.3. Реализуй метод init() представления. Он должен:
+    4.3.1. Вызывать инициализацию графического интерфейса initGui().
+    4.3.2. Добавлять слушателя событий нашего окна. В качестве подписчика создай и используй объект класса FrameListener.
+    В качестве метода для добавления подписчика используй подходящий метод из класса Window от которого наследуется и наш класс через классы JFrame и Frame.
+    4.3.3. Показывать наше окно. Используй метод setVisible с правильным параметром.
+    На этом этапе приложение при запуске должно показывать окно, которое можно растягивать, разворачивать, закрыть и т.д.
+     */
+    public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+
 }
