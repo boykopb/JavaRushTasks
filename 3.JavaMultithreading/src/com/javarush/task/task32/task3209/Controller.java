d67ba31b991f5bf36403ed4e20d831efa82dcaf0
Pavel Boyko <boykopb@gmail.com>
2020-09-23 20:58:39 +0300

    23.09.2020 20.58

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.StringReader;
 
 public class Controller {
 
@@ -27,13 +29,38 @@ public class Controller {
         return document;
     }
 
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+
+    }
+
+    public void setPlainText(String text){
+        resetDocument();
+        try {
+            StringReader stringReader = new StringReader(text);
+            new HTMLEditorKit().read(stringReader, document,0);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+
+
+    }
+
     public static void main(String[] args) {
         View view = new View();
-        Controller controller= new Controller(view);
+        Controller controller = new Controller(view);
         view.setController(controller);
         view.init();
         controller.init();
 
     }
+
+
 
 }
