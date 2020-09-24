829ad4852da4598ce73afad8f5521d3aa8b783cc
Pavel Boyko <boykopb@gmail.com>
2020-09-24 21:02:30 +0300

    24.09.2020 21.05

--- 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -1,6 +1,7 @@
 package com.javarush.games.spaceinvaders;
 
 import com.javarush.engine.cell.*;
+import com.javarush.games.spaceinvaders.gameobjects.Bullet;
 import com.javarush.games.spaceinvaders.gameobjects.EnemyFleet;
 import com.javarush.games.spaceinvaders.gameobjects.Star;
 
@@ -13,30 +14,31 @@ public class SpaceInvadersGame extends Game {
     private List<Star> stars;
     private EnemyFleet enemyFleet;
     public static final int COMPLEXITY = 5;
+    private List<Bullet> enemyBullets;
 
     private void drawField() {
-
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
                 setCellValueEx(x, y, Color.BLACK, "");
             }
         }
-
-        for (int i = 0; i < stars.size(); i++) {
-            stars.get(i).draw(this);
-
+        for (Star star : stars) {
+            star.draw(this);
         }
-
     }
 
     private void drawScene() {
         drawField();
         enemyFleet.draw(this);
+        for (Bullet bullet : enemyBullets) {
+            bullet.draw(this);
+        }
     }
 
     private void createGame() {
         createStars();
         enemyFleet = new EnemyFleet();
+        enemyBullets = new  ArrayList<Bullet>();
         drawScene();
         setTurnTimer(40);
     }
@@ -59,10 +61,29 @@ public class SpaceInvadersGame extends Game {
     @Override
     public void onTurn(int step) {
         moveSpaceObjects();
+        check();
+        Bullet fireShot = enemyFleet.fire(this);
+        if (fireShot != null) enemyBullets.add(fireShot);
         drawScene();
     }
 
     private void moveSpaceObjects() {
         enemyFleet.move();
+        for (Bullet bullet : enemyBullets) {
+            bullet.move();
+        }
+    }
+
+    private void removeDeadBullets() {
+        List<Bullet> enemyBulletsCopy = enemyBullets;
+        for (Bullet bullet : enemyBulletsCopy) {
+            if (bullet.y >= HEIGHT - 1) {
+                enemyBullets.remove(bullet);
+            }
+        }
+    }
+
+    private void check() {
+        removeDeadBullets();
     }
 }
