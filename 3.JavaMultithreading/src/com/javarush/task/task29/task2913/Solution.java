f8f6ab49e90f547dde285b7a3f552766eb711964
Pavel Boyko <boykopb@gmail.com>
2020-09-25 18:46:20 +0300

    25.09.2020 19.00

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task29.task2913;
+
+import java.util.Random;
+
+/* 
+Замена рекурсии
+*/
+
+public class Solution {
+    private static int numberA;
+    private static int numberB;
+
+    public static String getAllNumbersBetween(int a, int b) {
+        StringBuilder result = new StringBuilder();
+        if (a > b) {
+            for (int i = b; i < a; i++) {
+                result.append(i).append(" ");
+            }
+
+        }
+        if (a == b) {
+            result.append(b);
+        }
+        if (a < b){
+            for (int i = b; i > a; i--) {
+                result.append(i).append(" ");
+            }
+        }
+
+        return result.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        numberA = random.nextInt(1000);
+        numberB = random.nextInt(1000);
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+}
