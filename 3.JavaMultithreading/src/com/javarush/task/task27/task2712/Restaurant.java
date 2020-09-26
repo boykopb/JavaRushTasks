23960b16172c964c2fd1afed2067f0b4943f2311
Pavel Boyko <boykopb@gmail.com>
2020-09-26 15:32:55 +0300

    26.09.2020 15.30

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,17 +1,22 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
-import com.javarush.task.task27.task2712.kitchen.Dish;
-import com.javarush.task.task27.task2712.kitchen.Order;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Restaurant {
 
     public static void main(String[] args) throws IOException {
-        Tablet tablet = new Tablet(1);
+        Waiter waiter = new Waiter();
         Cook cook = new Cook("John Smith");
+
+        Tablet tablet = new Tablet(1);
+        cook.addObserver(waiter);
+
         tablet.addObserver(cook);
+
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
