2210033980eafa72e8a31e3977205707f59091c8
Pavel Boyko <boykopb@gmail.com>
2020-10-01 11:11:36 +0300

    01.10.2020 11.00

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -11,14 +11,21 @@ import java.util.regex.Pattern;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+        BigInteger bigInteger;
         try {
             for (int i = 2; i <= 36; i++) {
-                BigDecimal bi = new BigDecimal(new BigInteger(args[0], i));
-
+                try {
+                    bigInteger = new BigInteger(args[0], i);
+                    System.out.println(i);
+                    return;
+                } catch (Exception e) {
+                }
             }
-        } catch (Exception e) {
             System.out.println("incorrect");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
         }
     }
 }
