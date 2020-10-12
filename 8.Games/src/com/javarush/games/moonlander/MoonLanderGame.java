8aef8b0bfe35d0b330e414b7a9a1ea256f7b239b
Pavel Boyko <boykopb@gmail.com>
2020-10-12 09:34:40 +0300

    12.10.2020 10.00

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -7,20 +7,23 @@ public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
+    private GameObject landscape;
 
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
+        showGrid(false);
     }
 
 
     private void createGame() {
-        rocket = new Rocket(WIDTH / 2, 0);
+        createGameObjects();
         drawScene();
+        setTurnTimer(50);
     }
-    
+
     private void drawScene() {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
@@ -29,6 +32,29 @@ public class MoonLanderGame extends Game {
 
         }
         rocket.draw(this);
+        landscape.draw(this);
+    }
+
+    private void createGameObjects() {
+        rocket = new Rocket(WIDTH / 2, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+
+    }
+
+    @Override
+    public void onTurn(int step) {
+        rocket.move();
+        drawScene();
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x > WIDTH - 1 || x < 0 || y < 0 || y > HEIGHT - 1) {
+            return;
+        }
+
+        super.setCellColor(x, y, color);
+
 
     }
 }
