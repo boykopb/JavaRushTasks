13728921cbe740df9715aea12f65051369f45724
Pavel Boyko <boykopb@gmail.com>
2020-11-04 08:20:40 +0300

    04.11.2020 8.20

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -5,11 +5,37 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
+
 
     @Override
     public void initialize() {
         showGrid(false);
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
     }
 
+
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        drawField();
+    }
+
+    private void drawField() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == CENTER_X) {
+                    setCellColor(x, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < WIDTH - ROADSIDE_WIDTH) {
+                    setCellColor(x, y, Color.DIMGREY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
+    }
 }
